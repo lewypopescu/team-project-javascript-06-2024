d095deddfe5af98dcd884b00d7461f5dbfda46ee
lewypopescu <popesculewy@gmail.com>
2024-06-30 10:28:54 +0200

    done ok

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import './js/cards';
 import './js/dark-mode';
 import './js/spinner.js';
 import './js/search.js';
-import './js/apifetch.js'; // Importăm fișierul apifetch.js
+import './js/apifetch.js';
 import './js/pagination.js';
 import './js/modal.js';
 import './js/imports.js';
-import { setupPagination } from './js/apifetch.js'; // Importăm funcția setupPagination
-setupPagination(); // Inițializăm paginarea
+import { setupPagination } from './js/apifetch.js';
+setupPagination();
